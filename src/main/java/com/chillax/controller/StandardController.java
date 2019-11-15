04b00a17bdf79a6e0b379570cb9142082798b7c7
MAXWBLL <1850746556@qq.com>
2019-11-15 18:52:07 +0800

    重新写的代码，也许在报错，上一次提交错误的删除了一个分支，上一个分支本地OK

---
+++ src/main/java/com/chillax/controller/StandardController.java
@@ -0,0 +1,98 @@
+package com.chillax.controller;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.poi.ss.formula.functions.T;
+//这里不能引入对象
+//import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
+import com.chillax.dto.Standard;
+import com.chillax.service.StandardService;
+import com.sun.mail.iap.Response;
+
+import bease.BaseController;
+import bease.DiyExceetion;
+
+@SuppressWarnings("all")
+@Controller
+@Resource
+@ResponseBody
+public class StandardController extends BaseController {
+	@Autowired
+	private StandardService standardService;
+	Object standardOut;
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping("/standardAction_add")
+	public JSONObject addstandard(Standard standard, HttpServletRequest request, Model model)
+			throws InstantiationException, IllegalAccessException, InvocationTargetException {
+		JSONObject jsonObject = new JSONObject();
+		try {
+			standardService.addstandard(standard);
+			jsonObject.put("success", "success");
+		} catch (Exception e) {
+			jsonObject.put("error", "error");
+			System.out.println("json内容："+jsonObject.toString());
+			jsonObject.clear();
+			System.out.println("clear后的json内容："+jsonObject.toString());
+			jsonObject.put("error", "error");
+		}
+		return jsonObject;
+	}
+	// 保存数据
+		@RequestMapping("/add")
+		public Response add(@RequestBody T entity) {
+			//getBaseService().insert(entity);
+			return null ;//new Response().success(AppServiceHelper.getMessage("operateSuccess", new String[]{}, request.getLocale()));
+		}
+/**
+ * 测试类
+ */
+	public JSONObject test(Standard standard, HttpServletRequest request, Model model)
+			throws InstantiationException, IllegalAccessException, InvocationTargetException {
+		// 第一种方式使用实体类接收数据,实现方式不明
+		System.out.println("standard内容:"+standard.toString());
+		// 第二种方式使用model接收数据,实现方式不明,内容与实体类相同
+		System.out.println("model内容:"+model.toString());
+		// 第三种方式使用serverlet获取单个字段
+		standard.setId(Integer.valueOf(request.getParameter("id")));
+		standard.setMaxLength(Double.valueOf(request.getParameter("maxLength")));
+		// 第四种方式使用servicelet获取map
+		Map<String, Object> map = getMap(request.getParameterMap());
+		for (Entry<String, Object> entry : map.entrySet()) {
+			System.out.println("key=" + entry.getKey() + "\tvalue=" + entry.getValue());
+		}
+		try {
+			Integer seconds = (int) (System.currentTimeMillis() / 1000);
+			Object standardOut = mapToBean(map, standard);
+			
+
+		} catch (IllegalArgumentException | DiyExceetion e) {
+			System.out.println("test-----fail");
+			e.printStackTrace();
+		}
+
+		JSONObject jsonObject = new JSONObject();
+
+		try {
+			standardService.addstandard(standardOut);
+			jsonObject.put("success", "success");
+
+		} catch (Exception e) {
+			jsonObject.put("error", "error");
+		}
+		return jsonObject;
+	}
+}
--- src/main/java/com/chillax/dto/Standard.java
+++ src/main/java/com/chillax/dto/Standard.java
@@ -5,72 +5,138 @@ import java.util.Date;
 public class Standard {
 	private Integer id; // 主键
 	private String name; // 标准名称
-	private Integer minWeight; // 最小重量
-	private Integer maxWeight; // 最大重量
-	private Integer minLength; // 最小长度
-	private Integer maxLength; // 最大重量
+	private Double minWeight; // 最小重量
+	private Double maxWeight; // 最大重量
+	private Double minLength; // 最小长度
+	private Double maxLength; // 最大重量
 	private Date operatingTime;// 操作时间
 	private String operator; // 操作员
 	private String operatingCompany; // 操作单位
+	private String creator;// 创建责任者
+	private Date create_time;// 创建时间
+	private String revisor;// 修改人
+	private Date revice_time;// 修改时间
+	private String verison;// 版本号
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Integer getMinWeight() {
+
+	public Double getMinWeight() {
 		return minWeight;
 	}
-	public void setMinWeight(Integer minWeight) {
+
+	public void setMinWeight(Double minWeight) {
 		this.minWeight = minWeight;
 	}
-	public Integer getMaxWeight() {
+
+	public Double getMaxWeight() {
 		return maxWeight;
 	}
-	public void setMaxWeight(Integer maxWeight) {
+
+	public void setMaxWeight(Double maxWeight) {
 		this.maxWeight = maxWeight;
 	}
-	public Integer getMinLength() {
+
+	public Double getMinLength() {
 		return minLength;
 	}
-	public void setMinLength(Integer minLength) {
+
+	public void setMinLength(Double minLength) {
 		this.minLength = minLength;
 	}
-	public Integer getMaxLength() {
+
+	public Double getMaxLength() {
 		return maxLength;
 	}
-	public void setMaxLength(Integer maxLength) {
+
+	public void setMaxLength(Double maxLength) {
 		this.maxLength = maxLength;
 	}
+
 	public Date getOperatingTime() {
 		return operatingTime;
 	}
+
 	public void setOperatingTime(Date operatingTime) {
 		this.operatingTime = operatingTime;
 	}
+
 	public String getOperator() {
 		return operator;
 	}
+
 	public void setOperator(String operator) {
 		this.operator = operator;
 	}
+
 	public String getOperatingCompany() {
 		return operatingCompany;
 	}
+
 	public void setOperatingCompany(String operatingCompany) {
 		this.operatingCompany = operatingCompany;
 	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public Date getCreate_time() {
+		return create_time;
+	}
+
+	public void setCreate_time(Date create_time) {
+		this.create_time = create_time;
+	}
+
+	public String getRevisor() {
+		return revisor;
+	}
+
+	public void setRevisor(String revisor) {
+		this.revisor = revisor;
+	}
+
+	public Date getRevice_time() {
+		return revice_time;
+	}
+
+	public void setRevice_time(Date revice_time) {
+		this.revice_time = revice_time;
+	}
+
+	public String getVerison() {
+		return verison;
+	}
+
+	public void setVerison(String verison) {
+		this.verison = verison;
+	}
+
 	@Override
 	public String toString() {
 		return "Standard [id=" + id + ", name=" + name + ", minWeight=" + minWeight + ", maxWeight=" + maxWeight
 				+ ", minLength=" + minLength + ", maxLength=" + maxLength + ", operatingTime=" + operatingTime
-				+ ", operator=" + operator + ", operatingCompany=" + operatingCompany + "]";
+				+ ", operator=" + operator + ", operatingCompany=" + operatingCompany + ", creator=" + creator
+				+ ", create_time=" + create_time + ", revisor=" + revisor + ", revice_time=" + revice_time
+				+ ", verison=" + verison + "]";
 	}
 
 }
